937fbd34a504241bf062cc93855cb3e1671c1223
NarcizzeN <47037769+NarcizzeN@users.noreply.github.com>
2022-11-01 21:36:48 +0000

    added cool image effect

--- components/ImageCreation.js
+++ components/ImageCreation.js
@@ -1,8 +1,22 @@
-import React from "react";
-import {motion} from "framer-motion";
+import { motion } from "framer-motion";
 
-const ImageCreation = ({src, alt}) => {
-  return <motion.img src={src} alt={alt}></motion.img>;
+//create a function with children that returns a motion.div
+const ImageCreation = ({ children }) => {
+
+  const variants = {
+    visible: { opacity: 1, y: 0, transition: { duration: 0.5 } },
+    hidden: { opacity: 0, y: 100 },
+  };
+
+  return (
+    //create a motion div that rotates 180 degrees
+    <>
+      <br />
+      <motion.div initial={variants.hidden} whileInView={variants.visible}>
+        {children}
+      </motion.div>
+    </>
+  );
 };
 
 export default ImageCreation;
