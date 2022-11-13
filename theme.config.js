df06a56d09456724798548236727ba86084a0b13
NarcizzeN <47037769+NarcizzeN@users.noreply.github.com>
2022-11-13 01:44:13 +0000

    deleted the blog part of the site. modified the footer.

--- theme.config.js
+++ theme.config.js
@@ -9,15 +9,6 @@ const Logo = ({ height }) => (
   </svg>
 );
 
-const Vercel = ({ height = 20 }) => (
-  <svg height={height} viewBox="0 0 283 64" fill="none">
-    <path
-      fill="currentColor"
-      d="M141.04 16c-11.04 0-19 7.2-19 18s8.96 18 20 18c6.67 0 12.55-2.64 16.19-7.09l-7.65-4.42c-2.02 2.21-5.09 3.5-8.54 3.5-4.79 0-8.86-2.5-10.37-6.5h28.02c.22-1.12.35-2.28.35-3.5 0-10.79-7.96-17.99-19-17.99zm-9.46 14.5c1.25-3.99 4.67-6.5 9.45-6.5 4.79 0 8.21 2.51 9.45 6.5h-18.9zM248.72 16c-11.04 0-19 7.2-19 18s8.96 18 20 18c6.67 0 12.55-2.64 16.19-7.09l-7.65-4.42c-2.02 2.21-5.09 3.5-8.54 3.5-4.79 0-8.86-2.5-10.37-6.5h28.02c.22-1.12.35-2.28.35-3.5 0-10.79-7.96-17.99-19-17.99zm-9.45 14.5c1.25-3.99 4.67-6.5 9.45-6.5 4.79 0 8.21 2.51 9.45 6.5h-18.9zM200.24 34c0 6 3.92 10 10 10 4.12 0 7.21-1.87 8.8-4.92l7.68 4.43c-3.18 5.3-9.14 8.49-16.48 8.49-11.05 0-19-7.2-19-18s7.96-18 19-18c7.34 0 13.29 3.19 16.48 8.49l-7.68 4.43c-1.59-3.05-4.68-4.92-8.8-4.92-6.07 0-10 4-10 10zm82.48-29v46h-9V5h9zM36.95 0L73.9 64H0L36.95 0zm92.38 5l-27.71 48L73.91 5H84.3l17.32 30 17.32-30h10.39zm58.91 12v9.69c-1-.29-2.06-.49-3.2-.49-5.81 0-10 4-10 10V51h-9V17h9v9.2c0-5.08 5.91-9.2 13.2-9.2z"
-    />
-  </svg>
-);
-
 export default {
   projectLink: "https://github.com/vercel/swr",
   docsRepositoryBase: "https://github.com/vercel/swr-site/blob/master/pages",
@@ -25,16 +16,18 @@ export default {
   unstable_flexsearch: true,
   // feedbackLink: "Question? Give us feedback →",
   feedbackLabels: "feedback",
-  logo: () =>  (
-      <>
-        <Logo height={12} />
-        <span
-          className="mx-2 font-extrabold hidden md:inline select-none"
-          title={"Multizoa - Explaining human society's place in the biological universe"}
-        >
-          Multizoa
-        </span>
-      </>
+  logo: () => (
+    <>
+      <Logo height={12} />
+      <span
+        className="mx-2 font-extrabold hidden md:inline select-none"
+        title={
+          "Multizoa - Explaining human society's place in the biological universe"
+        }
+      >
+        Multizoa
+      </span>
+    </>
   ),
   head: ({ title, meta }) => {
     const { route } = useRouter();
@@ -103,16 +96,8 @@ export default {
     );
   },
   footerText: () => (
-          <a
-            href="https://vercel.com/?utm_source=swr"
-            target="_blank"
-            rel="noopener"
-            className="inline-flex items-center no-underline text-current font-semibold"
-          >
-            <span className="mr-1">Creative Commons</span>
-            <span>
-              <Vercel />
-            </span>
-          </a>
-        )
-    }
+    <p className="inline-flex items-center no-underline text-current font-semibold">
+      <span className="mr-1">Creative Commons © 2022 Multizoa</span>
+    </p>
+  ),
+};
