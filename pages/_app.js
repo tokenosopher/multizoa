0f2bbb05d3068d047ff793d5618da1ffe6a54147
Narcis - Razer 2023 <narcis@marinc.at>
2023-01-28 14:36:19 +0000

    removed import "react-star-sky/dist/index.css"; from _app

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,5 @@
 import "../styles.css";
 import "nextra-theme-docs/style.css";
-import "react-star-sky/dist/index.css";
 
 export default function Nextra({ Component, pageProps }) {
   const getLayout = Component.getLayout || ((page) => page);
